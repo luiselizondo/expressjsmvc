2066a256aa33d9654cd1b2f103b98561ad6955bd
Luis Elizondo <lelizondo@gmail.com>
2014-03-01 11:53:43 -0600

    Modify scripts to reflect the name of the project and remove unnecesary scripts

--- scripts/install-init-ubuntu.sh
+++ scripts/install-init-ubuntu.sh
@@ -4,10 +4,10 @@ echo "Installing pm2"
 sudo npm install -g pm2
 
 echo "Creating init script"
-sudo cp init-ubuntu /etc/init.d/simplelogs
-sudo chmod a+x /etc/init.d/simplelogs
-sudo update-rc.d simplelogs defaults
+sudo cp init-ubuntu /etc/init.d/expressmvc
+sudo chmod a+x /etc/init.d/expressmvc
+sudo update-rc.d expressmvc defaults
 
 echo "Attemping to start SimpleLogs"
-sudo service simplelogs start
+sudo service expressmvc start
 
